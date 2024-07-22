eb5e8bdd759df9ca0c42f9f6416fdff7cb407088
Bryan Alcantara <bryanalcantara@Bryans-MacBook-Air.local>
2024-07-22 16:52:33 -0400

    backend almost completed just need to create some verfication methods

--- Pursuit-To-Fitness-Baackend/controllers/workoutsController.js
+++ Pursuit-To-Fitness-Baackend/controllers/workoutsController.js
@@ -1,29 +1,35 @@
 const express = require("express");
 const router = express.Router();
 const workoutArray = require("../models/workout");
+const { getAllWorkouts, getWorkout } = require("../queries/workout");
 
 // GET ALL WORKOUTS
-router.get("/", (req, res) => {
-  res.status(200).send(workoutArray);
+router.get("/", async (req, res) => {
+  const allWorkouts = await getAllWorkouts();
+  if (allWorkouts[0]) {
+    res.status(200).json(allWorkouts);
+  } else {
+    res.status(500).json({ error: "server error" });
+  }
 });
 
 
 // GET A SINGLE WORKOUT
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
   const { id } = req.params;
-  const workout = workoutArray.find(el => el.id === +id)
+  const workout = await getWorkout(id);
   if (workout) {
-    res.status(200).send(workout)
+    res.json(workout);
   } else {
-    res.status(404).json({error: `Workout with id: ${id} not found!`})
+    res.status(404).json({ error: "not found" });
   }
-})
+});
 
 // CREATE A NEW WORKOUT
-router.post("/", (req, res) => {
-  const currentWorkout = {id: workoutArray.length + 1, ...req.body}
-  workoutArray.push(currentWorkout)
-})
+router.post("/", checkBoolean, checkName, async (req, res) => {
+  const color = await createColor(req.body);
+  res.json(color);
+});
 
 // DELETE A WORKOUT 
 router.delete("/:id", (req, res) => {
